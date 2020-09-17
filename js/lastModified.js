2557d2177ed73770e0b0b29e6a85d412b53d1fb2
mizuki714 <hun18035@byui.edu>
2020-09-17 17:54:17 -0500

    nope

--- js/lastModified.js
+++ js/lastModified.js
@@ -1,3 +1,3 @@
-//const options = {weekday: 'long', day: 'numeric', month:'long', year: 'numeric'};
-//document.getElementById('lastModifiedDate').textContent = new Date().toLocaleDateString('en-US', options);
-var string = document.lastModified;
+const options = {weekday: 'long', day: 'numeric', month:'long', year: 'numeric'};
+document.getElementById('lastModifiedDate').textContent = new Date().toLocaleDateString('en-US', options);
+//var string = document.lastModified;

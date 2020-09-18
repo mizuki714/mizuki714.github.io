e8ac61b8b4c9e65e3cccfbdda289b6ab9f4175ee
mizuki714 <hun18035@byui.edu>
2020-09-18 13:26:06 -0500

    edits

--- js/lastModified.js
+++ js/lastModified.js
@@ -1,3 +1,3 @@
+//shows date last modified
 const options = {weekday: 'long', day: 'numeric', month:'long', year: 'numeric'};
 document.getElementById('lastModifiedDate').textContent = new Date().toLocaleDateString('en-US', options);
-//var string = document.lastModified;

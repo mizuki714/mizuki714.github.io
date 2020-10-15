34b9c3348ecd46690dc44963f7e8234d91dc7b79
mizuki714 <hun18035@byui.edu>
2020-10-15 15:57:39 -0500

    changes

--- lesson05/js/banner.js
+++ lesson05/js/banner.js
@@ -1,35 +1,5 @@
-var dismissibleItem = function (el, type, value) {
-    var html = '<span>' + value + '<button type="button" class="close>x</button></span>';
-    el.classList.add('dismissible', 'dismissible-' + type);
-    el.innerHTML = html;
-};
-el.removeAttribute('data-component');
-el.removeAttribute('data-type');
-el.removeAttribute('data-value');
-
-function dismissiveBanner(event) {
-    var height = el.offsetHeight,
-        opacity = 1,
-        timeout = null;
-
-    function reduceHeight() {
-        height -= 2;
-        el.setAttribute('Style', 'height: ' + height + 'px; opacity: ' + opacity);
-        if (height <= 0) {
-            window.clearInterval(timeout);
-            timeout = null;
-            el.remove();
-        }
-    }
-
-    function fade() {
-        opacity -= .1;
-        el.setAttribute('style', 'opacity: ' + opacity);
-        if (opacity <= 0) {
-            window.clearInterval(timeout);
-            timeout = window.setInterval(reduceHeight, 1)
-        }
-    }
-    timeout = window.setInterval(fade, 25);
+let date = new Date();
+date.getDay()
+if (date.getDay()==6){
+    document.querySelector(".banner").style.display="block" 
 }
-el.querySelector('.close').addEventListener('click', dismissiveBanner);

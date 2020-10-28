93d650c1aaf87bf748e3586a8bf1f96521952576
mizuki714 <hun18035@byui.edu>
2020-10-28 10:49:23 -0500

    lazy loader sucks

--- lesson07/js/lazyload.js
+++ lesson07/js/lazyload.js
@@ -1,29 +1,24 @@
-
-const images = document.querySelectorAll("[data-src]");
-
-function preloadImage(img){
-    const src = img.getAttribute("data-src");
-    if(!src){
-        return;
-    }
-    img.src = src;
-}
-
+const imagesLoading = document.querySelectorAll("img[data-src]");
+//parameters for intersection observer
 const imageOptions = {
-    threshold:0,
-    rootMargin: "0px, 0px, 200px 0px"
+    threshold: 0,
+    rootMargin: "0px, 0px, 50px 0px"
 };
-const imageObserver = new IntersectionObserver((entries, imgObserver) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            return;
-        } else {
-            preloadImage(entry.target);
-            imageObserver.unobserve(entry.target);
-        }
-    })
-}, imageOptions);
 
-images.forEach(image => {
-    imageObserver.observe(image);
-});
+const preloadImage = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src');
+    };
+};
+
+if ('IntersectionObserver' in window) {
+    const imageObserver = new IntersectionObserver((entries, observer) => {
+        entries.forEach((entry) => {});
+    }, imageOptions);
+
+
+    imagesLoading.forEach(img => {
+        imageObserver.observe(img);
+    });
+} else {}

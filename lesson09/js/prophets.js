c172be6a0efe876aed5978cb551efbc2fef2b25e
mizuki714 <hun18035@byui.edu>
2020-11-10 14:29:25 -0600

    prophets finish

--- lesson09/js/prophets.js
+++ lesson09/js/prophets.js
@@ -11,34 +11,37 @@ fetch(requestURL)
     .then(function (jsonObject) {
         console.table(jsonObject); // temporary checking for valid response and data parsing
         const prophets = jsonObject['prophets'];
+
+
+        //for loop to loop through the code and create cards
+        for (let i = 0; i < prophets.length; i++) {
+            let card = document.createElement('section');
+            let h2 = document.createElement('h2');
+
+
+            h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+
+            card.appendChild(h2);
+
+            document.querySelector('div.cards').appendChild(card);
+
+
+                let p1 = document.createElement('p');
+
+                p1.textContent = "Date of Birth: " + prophets[i].birthdate;
+
+                 card.appendChild(p1);
+
+                 let p2 = document.createElement('p');
+
+                 p2.textContent = "Place of Birth: " + prophets[i].birthplace;
+ 
+                  card.appendChild(p2);
+
+            let image = document.createElement('img');
+
+                image.setAttribute('src', prophets[i].imageurl);
+                card.appendChild(image);
+                
+        }
     });
-
-//for loop to loop through the code and create cards
-for (let i = 0; i < prophets.length; i++) {
-    let card = document.createElement('section');
-    let h2 = document.createElement('h2');
-
-
-    h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-
-    card.appendChild(h2);
-
-    document.querySelector('div.cards').appendChild(card);
-
-/*
-
-    let p = document.createElement('p');
-
-    p.textContent = "Date of Birth: " + prophets[i].birthdate +
-     "Place of Birth: " + prophets[i].birthplace;
-
-     card.appendChild(p);
-
-
-
-let image = document.createElement('src');
-
-    image.setAttribute('src', prophets[i].imageurl);
-    card.appendChild(image);
-     */
-}

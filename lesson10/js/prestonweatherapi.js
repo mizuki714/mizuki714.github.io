d0829c5cb63f4a162bf1db2345a78f17bfae5b6d
mizuki714 <hun18035@byui.edu>
2020-11-18 17:56:57 -0600

    works kinds

--- lesson10/js/prestonweatherapi.js
+++ lesson10/js/prestonweatherapi.js
@@ -26,45 +26,45 @@ fetch(apiURL)
         document.getElementById('highTemp').textContent = highFahren;
         document.getElementById('humidity').textContent =jsObject.main.humidity;
         document.getElementById('windSpeedMPH').textContent =jsObject.wind.speed;
+/*
 //5-day forecast
 var list1 = jsObject["list"];
 var dIDay = ["d1Day", "d2Day", "d3Day", "d4Day", "d5Day"];
 var dITemp =["d1Temp","d2Temp", "d3Temp", "d4Temp", "d5Temp" ];
 var iconArray = [];
 var dayArray = [];
-var tempArray =[ ];
+var tempArray =[];
 //let dayNames = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 //let dayName = dayNames[d.getDay()];
-
 //let d =new Date();
-
 for (let i = 0; i < list1.length; i++) {
     //IF to filter by 18:00
     if (list1[i].dt_txt.includes("18:00:00")) {
-​
         tempArray[i] = list1[i].main.temp;
         dayArray[i] = list1[i].dt_txt;
         iconArray[i] = list1[i].weather[0].icon;
         console.log(iconArray[i]);
 ​
-        for(i =0; i < tempArray.length; i++){
+        for(i = 0; i < tempArray.length; i++){
             tempArray[i] = dITemp[i];
         }
         // for (x =0; x < dITemp.length; i++) {
-​
+
         // }
+        for(x=0; x< dayArray.length; x++){
+
+        }
+        for(y = 0; y < iconArray.length; y++){
+
+        }
 ​
     }
     // store dt_tx in dIday that coordinated with it's new array location (ie..first loop answer need to go in d1Day, second in d2Day...)
 ​
     // store temp in dITemp that coordinates with it's new array location
+    
 ​
-​
-​
-}
-
-
-
-    });
+}*/
+});
 
 

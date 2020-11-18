cdb1e8e79a25d193ae901b2d3192b825cb2d6bd6
mizuki714 <hun18035@byui.edu>
2020-11-18 10:19:59 -0600

    edits

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,6 +1,6 @@
 /* key = 08a952b25f428f198a70d56f6b821a3f
 Preston cityid = "5604473"*/
-
+  /* api-test page */
 const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&appid=08a952b25f428f198a70d56f6b821a3f";
 fetch(apiURL)
   .then((response) => response.json())
@@ -16,3 +16,7 @@ fetch(apiURL)
     document.getElementById('icon').setAttribute('src', imagesrc); // focus on the setAttribute() method
     document.getElementById('icon').setAttribute('alt', desc);
   });
+
+  /* preston page */
+
+  

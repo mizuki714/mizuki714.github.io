7a452bea72536d9b6ba05344a9dab97243b475c2
mizuki714 <hun18035@byui.edu>
2020-11-18 15:23:24 -0600

    things that dont work

---
+++ lesson10/js/prestonweatherapi.js
@@ -0,0 +1,32 @@
+/* key = 08a952b25f428f198a70d56f6b821a3f
+Preston cityid = "5604473"*/
+/* api-page for Preston Page Weather */
+
+/* preston page */
+
+//weather summary
+const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&appid=08a952b25f428f198a70d56f6b821a3f";
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+        let kelvin = jsObject.main.temp;
+        let cel = kelvin - 273;
+        let fahrenheit = Math.floor(cel * (9 / 5) + 32);
+        document.getElementById('currentTemp').textContent = fahrenheit;
+        const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png'; // note the concatenation
+        const desc = jsObject.weather[0].description; // note how we reference the weather array
+        //Delete this line to show icon
+        document.getElementById('icon').setAttribute('src', imagesrc); // focus on the setAttribute() method
+        document.getElementById('icon').setAttribute('alt', desc);
+    });
+
+id = "currently"
+id = "highTemp"
+
+
+
+id = "humidity"
+id = "windSpeedMPH"
+
+//5-day forecast

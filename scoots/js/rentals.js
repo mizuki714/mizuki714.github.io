38215eb035ff72d176204aa07966b99d7c439e17
mizuki714 <hun18035@byui.edu>
2020-12-03 21:41:12 -0600

    MADE IT WORK!!!!!! OH MY GOSH!!!! CELEBRATE!!!!

--- scoots/js/rentals.js
+++ scoots/js/rentals.js
@@ -1,5 +1,5 @@
 //references the JSON file
-const requestURL = '/data/scoots.json';
+const requestURL = "./data/scoots.json";
 //Method to fetch the JSON file,
 fetch(requestURL)
 
@@ -25,39 +25,39 @@ fetch(requestURL)
             card.appendChild(image);
 
             //car name
-            let h2 = document.createElement('h2');
-            h2.textContent = rentals[i].name;
-            card.appendChild(h2);
+            let h3 = document.createElement('h3');
+            h3.textContent = rentals[i].name;
+            card.appendChild(h3);
             document.querySelector('div.rentalcards').appendChild(card);
 
             //car description
             let p1 = document.createElement('p');
-            p1.textContent = rentals[i].desc;
-            card.appendChild(p);
+            p1.textContent = "Description: " + rentals[i].desc;
+            card.appendChild(p1);
 
             //car reserve price full
             let p2 = document.createElement('p');
             p2.textContent = "Half-Day (3-hr) Reservation Price: " + rentals[i].reservehalf;
-            card.appendChild(p);
+            card.appendChild(p2);
 
             //car reserve price half
             let p3 = document.createElement('p');
             p3.textContent = "Full Day Reservation Price: " + rentals[i].reservefull;
-            card.appendChild(p);
+            card.appendChild(p3);
 
             //rental price walk in half
             let p4 = document.createElement('p');
             p4.textContent = "Half-Day (3-hr) Walk-in Price: " + rentals[i].walkhalf;
-            card.appendChild(p);
+            card.appendChild(p4);
 
             //rental price walk in full
             let p5 = document.createElement('p');
             p5.textContent = "Full Day Walk-in Price: " + rentals[i].walkfull;
-            card.appendChild(p);
+            card.appendChild(p5);
 
             //maximum capacity
             let p6 = document.createElement('p');
             p6.textContent = "Max Persons: " + rentals[i].capacity;
-            card.appendChild(p);
+            card.appendChild(p6);
         }
     });
